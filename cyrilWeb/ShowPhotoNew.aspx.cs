b5c60ff16c39fc3fcdd08136b524fc918b6909f7
顾浩 <guh@oceansoft.com.cn>
2018-11-28 15:36:36 +0800

    新加了项目内容

---
+++ cyrilWeb/ShowPhotoNew.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cyrilWeb
+{
+    public partial class ShowPhotoNew : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<String> listss = new List<string>();
+            // 根目录虚拟路径
+            string virtualPath = Page.Request.ApplicationPath;
+            // 根目录绝对路径
+            string pathRooted = HostingEnvironment.MapPath(virtualPath);
+
+            FileInfo[] files = new DirectoryInfo(pathRooted + "Photos").GetFiles();
+            List<FileInfo> list = new List<FileInfo>(files);
+            list.Sort(new Comparison<FileInfo>(delegate (FileInfo a, FileInfo b)
+            {
+                return b.CreationTime.CompareTo(a.CreationTime);
+
+            }));
+            string html = "";
+            int i = 0;
+            foreach (FileInfo f in list)
+            {
+                i++;
+                Console.WriteLine(f.Name + "," + f.CreationTime);
+                html += "  <a href='Photos/" + f.Name + "'><div id='div" + i + "' runat='server' style='float: left; '><img id='img" + i + "' runat='server' src='Photos/" + f.Name + "' style='width: 99 %; height: 500px; ' /></div> </a>";// this.img1.Src = "Photos/" + f.Name;
+
+            }
+            this.form1.InnerHtml = html;
+
+        }
+    }
+}
---
+++ cyrilWeb/updateLoad.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cyrilWeb
+{
+    public partial class updateLoad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void btnFileUpload_Click(object sender, EventArgs e)
+        {
+            Boolean fileOK = false;
+            //获取上传的文件名
+            string fileName = this.FileUpload1.FileName;
+            //获取物理路径
+            String path = Server.MapPath("~/Photos/");
+            //判断上传控件是否上传文件
+            if (FileUpload1.HasFile)
+            {
+                //判断上传文件的扩展名是否为允许的扩展名".gif", ".png", ".jpeg", ".jpg" ,".bmp"
+                String fileExtension = System.IO.Path.GetExtension(fileName).ToLower();
+                String[] Extensions = { ".gif", ".png", ".jpeg", ".jpg", ".bmp" };
+                for (int i = 0; i < Extensions.Length; i++)
+                {
+                    if (fileExtension == Extensions[i])
+                    {
+                        fileOK = true;
+                    }
+                }
+            }
+            //如果上传文件扩展名为允许的扩展名，则将文件保存在服务器上指定的目录中
+            if (fileOK)
+            {
+                try
+                {
+                    this.FileUpload1.PostedFile.SaveAs(path + fileName);
+                    MessageBox("文件上传完毕");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox("文件不能上传，原因：" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox("不能上传这种类型的文件");
+            }
+
+        }
+
+        protected void MessageBox(string str)
+        {
+            Page.ClientScript.RegisterStartupScript(Page.GetType(), "message", "<script language='javascript' defer>alert('" + str + "');</script>");
+        }
+    }
+}
